3e6f481a302f3df18ef30db20b60c11fae8bb0dc
Danny <202100352g@utea.edu.pe>
2024-03-14 10:16:57 -0500

    especialidades completo

--- app/Http/Controllers/SpecialtyController.php
+++ app/Http/Controllers/SpecialtyController.php
@@ -2,11 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Specialty;
 use Illuminate\Http\Request;
 
 class SpecialtyController extends Controller
 {
-    function index() {
-        return view('specialties.index');
+    public function __construct() {
+        $this->middleware('auth');
     }
+    public function index() {
+        $specialties = Specialty::all();
+        return view('specialties.index', compact('specialties'));
+    }
+    public function create() {
+        return view('specialties.create');
+    }
+
+    public function sendData(Request $request )  {
+        $speciality = new Specialty();
+        $speciality->name = $request->input('name');
+        $speciality->description = $request->input('description');
+        $speciality->save();
+
+        return redirect('/especialidades');
+    }
+
 }
